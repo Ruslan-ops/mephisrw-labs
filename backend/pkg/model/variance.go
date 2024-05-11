2e8311f2ae50d7d534d1dfe9a11ccc1a63f16681
yaakovlew <andrei252002@mail.ru>
2024-05-12 01:06:29 +0300

    upd: save variant models

--- backend/pkg/model/variance.go
+++ backend/pkg/model/variance.go
@@ -1,16 +1,16 @@
 package model
 
 type Variance1A struct {
-	Number int           `json:"number" binding:"required"`
-	Data   [][][]float64 `json:"data" binding:"required"`
+	Number int         `json:"number" binding:"required"`
+	Data   interface{} `json:"data" binding:"required"`
 }
 
 type Variance1B struct {
-	Number int           `json:"number" binding:"required"`
-	Data   [][][]float64 `json:"data" binding:"required"`
+	Number int         `json:"number" binding:"required"`
+	Data   interface{} `json:"data" binding:"required"`
 }
 
 type Variance2 struct {
-	Number int           `json:"number" binding:"required"`
-	Data   [][][]float64 `json:"data" binding:"required"`
+	Number int         `json:"number" binding:"required"`
+	Data   interface{} `json:"data" binding:"required"`
 }
